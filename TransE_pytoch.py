c2de599641d5530cbae427c09038b2f600494b03
Lxler <2499688499@qq.com>
2023-12-02 10:19:38 +0800

    finish

--- TransE_pytoch.py
+++ TransE_pytoch.py
@@ -586,40 +586,40 @@ class TransE:
 
 
 if __name__ == '__main__':
-    # file1 = "WN18/wordnet-mlj12-train.txt"
-    # file2 = "WN18/entity2id.txt"
-    # file3 = "WN18/relation2id.txt"
-    # file4 = "WN18/wordnet-mlj12-valid.txt"
-    # entity_set, relation_set, triple_list, valid_triple_list = dataloader(file1, file2, file3, file4)
-    #
-    # file5 = "WN18_torch_TransE_entity_50dim_batch4800"
-    # file6 = "WN18_torch_TransE_relation_50dim_batch4800"
-    #
-    # file8 = "WN18/wordnet-mlj12-test.txt"
-    # file9 = "Fb15k_loss_record.txt"
-    # transE = TransE(entity_set, relation_set, triple_list, embedding_dim=50, lr=0.01, margin=6.0, norm=1, C=0.25,
-    #                 valid_triple_list=valid_triple_list)
-    # transE.data_initialise()
-    # # transE.training_run(epochs=100, batch_size=4800,out_file_title="WN18_torch_")
-    # transE.insert_test_data(file5, file6, file8)
-    #
-    # transE.test_run(filter=True)
-
-    file1 = "FB15k/freebase_mtr100_mte100-train.txt"  # 就是把train.txt的尾实体和关系换个了位置，保证头实体、关系、尾实体的位置
-    file2 = "FB15k/entity2id.txt"
-    file3 = "FB15k/relation2id.txt"
-    file4 = "FB15k/freebase_mtr100_mte100-valid.txt"
-
-    file5 = "KB15k_torch_TransE_entity_50dim_batch9600"
-    file6 = "KB15k_torch_TransE_relation_50dim_batch9600"
-
-    file8 = "FB15k/freebase_mtr100_mte100-test.txt"
-    # file9 = "Fb15k_loss_record.txt"
+    file1 = "WN18/wordnet-mlj12-train.txt"
+    file2 = "WN18/entity2id.txt"
+    file3 = "WN18/relation2id.txt"
+    file4 = "WN18/wordnet-mlj12-valid.txt"
     entity_set, relation_set, triple_list, valid_triple_list = dataloader(file1, file2, file3, file4)
 
-    transE = TransE(entity_set, relation_set, triple_list, embedding_dim=50, lr=0.001, margin=4.0, norm=1, C = 0.25, valid_triple_list=valid_triple_list)
+    file5 = "WN18_torch_TransE_entity_50dim_batch4800"
+    file6 = "WN18_torch_TransE_relation_50dim_batch4800"
+
+    file8 = "WN18/wordnet-mlj12-test.txt"
+    #file9 = "Fb15k_loss_record.txt"
+    transE = TransE(entity_set, relation_set, triple_list, embedding_dim=50, lr=0.01, margin=6.0, norm=1, C=0.25,
+                    valid_triple_list=valid_triple_list)
     transE.data_initialise()
-    transE.training_run(epochs=500, batch_size=9600, out_file_title="KB15k_torch_")
+    transE.training_run(epochs=100, batch_size=4800,out_file_title="WN18_torch_")
     transE.insert_test_data(file5, file6, file8)
 
-    transE.test_run(filter=False)
+    transE.test_run(filter=True)
+
+    # file1 = "FB15k/freebase_mtr100_mte100-train.txt"  # 就是把train.txt的尾实体和关系换个了位置，保证头实体、关系、尾实体的位置
+    # file2 = "FB15k/entity2id.txt"
+    # file3 = "FB15k/relation2id.txt"
+    # file4 = "FB15k/freebase_mtr100_mte100-valid.txt"
+    #
+    # file5 = "KB15k_torch_TransE_entity_50dim_batch9600"
+    # file6 = "KB15k_torch_TransE_relation_50dim_batch9600"
+    #
+    # file8 = "FB15k/freebase_mtr100_mte100-test.txt"
+    # # file9 = "Fb15k_loss_record.txt"
+    # entity_set, relation_set, triple_list, valid_triple_list = dataloader(file1, file2, file3, file4)
+    #
+    # transE = TransE(entity_set, relation_set, triple_list, embedding_dim=50, lr=0.001, margin=4.0, norm=1, C = 0.25, valid_triple_list=valid_triple_list)
+    # transE.data_initialise()
+    # transE.training_run(epochs=500, batch_size=9600, out_file_title="KB15k_torch_")
+    # transE.insert_test_data(file5, file6, file8)
+    #
+    # transE.test_run(filter=False)
